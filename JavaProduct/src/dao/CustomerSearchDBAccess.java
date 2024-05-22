b6e3a39fa0b5324f5b78ff6ee2fd443714458bc4
nimo1706 <kokuri20000101@gmail.com>
2024-05-22 09:49:34 +0900

    commit #10

--- JavaProduct/src/dao/CustomerSearchDBAccess.java
+++ JavaProduct/src/dao/CustomerSearchDBAccess.java
@@ -2,7 +2,12 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Customer;
 
 public class CustomerSearchDBAccess {
 
@@ -16,19 +21,53 @@ public class CustomerSearchDBAccess {
 		} catch (ClassNotFoundException | SQLException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 		return con;
 	}
-	private Connection closeConnection(Connection con){
-		
-		
+
+	private Connection closeConnection(Connection con) {
+
 		try {
-			if(con != null) {
+			if (con != null) {
 				con.close();
 			}
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
 		return con;
+	}
+
+	public List<Customer> searchCustomerByTel(String tel) {
+		
+		String sql = "SELECT CUSTID, CUSTNAME, KANA, ADDRESS FROM customer;";
+		
+		try (Connection con = createConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);) {
+			
+			ResultSet rs = pstmt.executeQuery();
+			List<Customer> list = new ArrayList<>();
+			
+			while (rs.next()) {
+				int custId = rs.getInt("CUSTID");
+				String custName = rs.getString("CUSTNAME");
+
+				String kana = rs.getString("KANA");
+				String address = rs.getString("ADDRESS");
+
+				list.add(new Customer(custId, custName, kana, tel, address));
+				
+			}
+			
+			return list;
+
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+
+	public searchCustomerByKana(String kana){
+		
+		return void;
 	}
 }
